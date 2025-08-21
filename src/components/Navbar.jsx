a89069e9a7dc3ba2db7a6626873f7c4a8d0f2feb
Hugo <hugo.borrego02@gmail.com>
2025-08-21 13:28:54 -0300

    feat: Adicionando as rotas de navegação da página

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
@@ -8,20 +9,31 @@ const Navbar = () => {
           <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
           <polyline points="9 22 9 12 15 12 15 22" />
         </svg>
-        <h1 className="text-xl font-bold text-blue-600">Sua Voz na Escola</h1>
+        <NavLink to="/">
+          <h1 className="text-xl font-bold text-blue-600">Sua Voz na Escola</h1>
+        </NavLink>
+
       </div>
       <div className="space-x-4">
-        <button className="text-purple-600 font-semibold py-2 px-4 rounded-full border-2 border-purple-600 hover:bg-purple-50 transition duration-300">
-          Ver Denúncias Públicas
-        </button>
-        <button className="text-blue-600 font-semibold py-2 px-4 rounded-full border-2 border-blue-600 hover:bg-blue-50 transition duration-300">
-          Entrar
-        </button>
-        <button className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-full shadow hover:bg-blue-700 transition duration-300">
-          Criar Conta
-        </button>
+        <NavLink to="/seepubliccomplaints">
+          <button className="text-purple-600 font-semibold py-2 px-4 rounded-full border-2 border-purple-600 hover:bg-purple-50 transition duration-300">
+            Ver Denúncias Públicas
+          </button>
+        </NavLink>
+
+        <NavLink to="/login">
+          <button className="text-blue-600 font-semibold py-2 px-4 rounded-full border-2 border-blue-600 hover:bg-blue-50 transition duration-300">
+            Entrar
+          </button>
+        </NavLink>
+
+        <NavLink to="signup">
+          <button className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-full shadow hover:bg-blue-700 transition duration-300">
+            Criar Conta
+          </button>
+        </NavLink>
       </div>
-    </nav>
+    </nav >
   );
 };
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+const Login = () => {
+  return (
+    <>
+      <section className="pt-28 w-full max-w-3xl mx-auto">
+        <h1 className="flex justify-center mb-6 text-blue-600 text-3xl font-bold">
+          Tela de Login
+        </h1>
+      </section>
+    </>
+  );
+};
+
+export default Login;
