34aa431029e7f953e77694aef61ec7fbfc8ef0a2
Gabriel Senna <x.senna@gmail.com>
2025-08-21 22:05:50 -0300

    LoginPage adicionada, Página inicial alterada

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import HomePage from "./pages/HomePage.jsx";
 import Navbar from "./components/Navbar.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <SignUpPage />
+      <LoginPage />
     </>
   );
 };
